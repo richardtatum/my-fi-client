b53e15ee7b540d707104d7f7cb5a752bebb1de57
Richard Tatum <rtatum@pm.me>
2023-01-02 14:19:33 +0000

    DI registration

--- src/MyFi.TheBadlands/Startup.cs
+++ src/MyFi.TheBadlands/Startup.cs
@@ -1,5 +1,8 @@
-﻿using MyFi.TheBadlands.Database;
+﻿using MyFi.TheBadlands.Clients;
+using MyFi.TheBadlands.Database;
+using MyFi.TheBadlands.Models.Monzo;
 using MyFi.TheBadlands.Repositories;
+using MyFi.TheBadlands.Services;
 
 namespace MyFi.TheBadlands;
 
@@ -19,6 +22,15 @@ public class Startup
         services.AddSingleton<IDbConnectionConfig>(new SqliteConnectionConfig(Configuration["DatabaseName"]));
         services.AddScoped<CommandRepository>();
         services.AddScoped<QueryRepository>();
+
+        services.AddScoped<MonzoService>();
+        services.AddScoped<UserService>();
+
+        services.AddOptions<MonzoOptions>()
+            .BindConfiguration("Monzo");
+
+        services.AddHttpClient<MonzoClient>();
+        // Add retry policy
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
